6619337c6b462cc93b15b7a96d88dcbfb771ede3
NITO666 <lagun-01@mail.ru>
2022-02-24 22:33:15 +0300

    feat:feat task

---
+++ src/index.js
@@ -0,0 +1,37 @@
+module.exports = function check(str, bracketsConfig) {
+  let a1 = []
+  let b2 = []
+  let c = 0
+  let d = 0
+  let e = 0
+  let f = 0
+    for(let i = 0; i < str.length;i++){
+      a1.push(str[i].charCodeAt())
+      //c = c + str[i].charCodeAt() - str[i+1].charCodeAt()
+    }
+    for(let i = 0; i < bracketsConfig.length;i++){
+      for(let j = 0;j < bracketsConfig[i].length;j++){
+        b2.push(bracketsConfig[i][j].charCodeAt())
+        //d = d + bracketsConfig[i][j].charCodeAt() - bracketsConfig[i][j+1].charCodeAt()
+      }
+    }
+    a1 = a1.sort((a,b)=>a-b)
+    b2 = b2.sort((a,b)=>a-b)
+    for(let i = 0; i < a1.length/2;i++){
+      c = c + a1[i]
+    }
+    for(let i = a1.length/2; i < a1.length;i++){
+      d = d + a1[i]
+    }
+    for(let i = 0; i < b2.length/2;i++){
+      e = e + a1[i]
+    }
+    for(let i = b2.length/2; i < b2.length;i++){
+      f = f + a1[i]
+    }
+    if(c-d == e - f){
+      return true
+    }else{
+      return false
+    }
+}
